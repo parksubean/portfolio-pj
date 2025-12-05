4b8288224a1402b43576fbab96bfd1fbeb6c513c
[subean] <suqls10@gmail.com>
2025-12-05 14:56:47 +0900

    업데이트 및 재배포

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-   base: "/portfolio-pj/",
+   base: "./",
    plugins: [react()],
    test: {
       environment: "jsdom",
